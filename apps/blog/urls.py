a480c785b5fa4054aa88cce45aeb286a7be7f92b
llOriginaljuonll <lastsecond1989@gmail.com>
2024-01-29 05:31:24 +0700

    create blog_form page

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import blog_home
+from .views import blog_home, blog_form
 
 urlpatterns = [
     path('', blog_home, name='blog-home'),
+    path('blog/write/<str:name>', blog_form, name='blog-form'),
 ]
