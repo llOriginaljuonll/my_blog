977db401e458ccf486c5f9d67d877ce724025119
llOriginaljuonll <lastsecond1989@gmail.com>
2024-02-02 02:33:19 +0700

    add delete system

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import blog_home, blog_form, blog_detail, blog_edit
+from .views import blog_home, blog_form, blog_detail, blog_edit, blog_delete
 
 urlpatterns = [
     path('', blog_home, name='blog-home'),
     path('blog/write/<int:user_id>', blog_form, name='blog-form'),
     path('blog/edit/<int:blog_id>/', blog_edit, name='blog-edit'),
     path('blog/<int:blog_id>', blog_detail, name='blog-detail'),
+    path('blog/delete/<int:blog_id>', blog_delete, name='blog-delete'),
 ]
