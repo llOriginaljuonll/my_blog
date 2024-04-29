70c8cd46b54573df5176d6859bed837dfaecccce
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-04-30 02:09:10 +0700

    Install django-crispy-forms and Uninsatll crispy-bootstrap5

--- site_project/settings.py
+++ site_project/settings.py
@@ -44,12 +44,12 @@ INSTALLED_APPS = [
 
     # Third-party apps
     "crispy_forms",
-    "crispy_bootstrap5",
+    "crispy_tailwind",
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
