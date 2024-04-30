39a697b50ef25a8baf618931d57cd4979c2ac1c0
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-05-01 01:06:12 +0700

    Fix when register already user login and automatically and rename block base template

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from .forms import SignUpForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 class SignupView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('blog:blog-home')
     template_name = 'registration/sign_up.html'
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid
