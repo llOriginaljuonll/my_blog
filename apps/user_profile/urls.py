64960707698a093169b4f5c0e58a51818371998e
pond <lastsecond1989@gmail.com>
2024-05-15 23:45:38 +0700

    add url of user profile page

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProfileDetailView
 
 urlpatterns = [
-    path('<slug:slug>/', ProfileDetailView.as_view()),
+    path('<slug:slug>/', ProfileDetailView.as_view(), name='user_profile'),
 ]
