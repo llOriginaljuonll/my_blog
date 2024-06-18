806b18b1383ef7984289e5d77d76b969bf316409
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-06-18 08:37:35 +0700

    create user profile page

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
 from .models import Profile
-from django.views.generic import DetailView
+from apps.blog.models import Blog
+from django.views.generic import DetailView, TemplateView
+
 
 
 class ProfileDetailView(DetailView):
 
     model = Profile
     template_name = 'profile/user_profile.html'
+
+    def get_queryset(self):
+        pass
+        return super().get_queryset()
