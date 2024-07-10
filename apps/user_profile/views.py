5f9f55eb3be4250bf94f84bd54b8bd4fd5e28d6c
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-07-11 04:31:38 +0700

    create ProfileForm / add ProfileForm to user_profile--def, dashboard--def

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,9 +1,9 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
 from apps.blog.models import Blog
-from django.views.generic import DetailView, TemplateView
+from .forms import ProfileForm
 
    
 def profile_detail(request, slug):
@@ -11,11 +11,23 @@ def profile_detail(request, slug):
     bookmark_list = Blog.objects.filter(bookmark=request.user)
     user = Profile.objects.get(user__username=slug)
 
-    return render(request, "profile/user_profile.html", {'profile': user, 'blogs': bookmark_list})
+    instance = Profile.objects.get(id=request.user.pk)
+    form = ProfileForm(request.POST or None, instance=instance)
+    if form.is_valid():
+          form.save()
+          return redirect('profile:user-profile')
+
+    return render(request, "profile/user_profile.html", {'profile': user, 'blogs': bookmark_list, 'form': form})
 
 def dashboard(request):
 
     blogs = Blog.objects.all().order_by("-pk")
-    user = Profile.objects.get(user__id=request.user.id)
+    user = Profile.objects.get(user__id=request.user.pk)
 
-    return render(request, "dashboard.html", {'blogs': blogs, 'profile': user})
+    instance = Profile.objects.get(id=request.user.pk)
+    form = ProfileForm(request.POST or None, instance=instance)
+    if form.is_valid():
+          form.save()
+          return redirect('profile:user-profile')
+
+    return render(request, "dashboard.html", {'blogs': blogs, 'profile': user, 'form': form})
